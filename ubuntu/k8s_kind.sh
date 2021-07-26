5b2398c4ca94d7b5c47e4cbe7243887d3563e592
Will Dalgleish <will.dalgleish@outlook.com>
2021-07-26 20:24:27 +0100

    Script for Kind K8s

---
+++ ubuntu/k8s_kind.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Installs Kind to run kubectl on docker
+
+#install kubectl
+sudo snap install kubectl --classic
+
+#add alias for kubectl
+#not required
+alias k="kubectl"
+
+#install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+#add to path
+mv ./kind /usr/loca/bin/kind
+
