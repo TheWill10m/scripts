31630c238a5597490ef9bdf96791b0a4b7c6037c
Will Dalgleish <will.dalgleish@outlook.com>
2021-07-26 20:51:47 +0100

    More -y for apt install

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Prerequisites and add repository
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install -yapt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update
 
 #Install
-apt install docker-ce -y
+apt install -y docker-ce
 
 #Add user to docker group
 #Not required
